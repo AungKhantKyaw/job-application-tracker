eca54c519e038c0bfe0db71c1e9cdba8479577d6
AK <AungKhantKyaw@users.noreply.github.com>
2026-08-26 16:59:34 +1200

    update the security vulnerabilities and configurations
    update the security vulnerabilities and configurations

--- job_tracker/settings.py
+++ job_tracker/settings.py
@@ -21,12 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^6n!12n)z+5)!m)a5dm88+wfhv7k^4!on=)d_3lqf10oa-z&(z'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
 # Initialize environment variables
 env = environ.Env(
     DEBUG=(bool, False)
@@ -35,7 +29,13 @@ env = environ.Env(
 # Read .env file
 environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 
-ALLOWED_HOSTS = []
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = env('SECRET_KEY')
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = env.bool('DEBUG', default=False)
+
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=[])
 
 
 # Application definition
